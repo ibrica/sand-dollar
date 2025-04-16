b9b8f1521b173525172a862e80b93be92d0f7716
ibrica <ivbritvic@gmail.com>
2025-04-16 14:19:54 +0200

    fix: config and layout file

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { WalletProvider } from '@/components/WalletProvider';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
